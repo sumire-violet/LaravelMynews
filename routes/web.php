db14aefde760a9c68562a3b647ca3af2258dd216
sumire-violet <blackout.violet.k@gmail.com>
2021-12-18 08:07:50 +0000

    21章

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::get('profile/create','Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
 });
 Route::get('/XXX','Admin\AAAController@bbb');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
