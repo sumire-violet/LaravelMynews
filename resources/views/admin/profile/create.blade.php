0cc62ffa0441674289fd9edf5c10607a7a810b9f
sumire-violet <blackout.violet.k@gmail.com>
2021-12-18 06:06:06 +0000

    20章やり直し∞∞∞

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -4,8 +4,11 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>Laravel</title>
     </head>
     <body>
+        
     </body>
 </html>
 {{-- layouts/plofile.blade.phpを読み込む --}}
@@ -13,4 +16,11 @@
 {{-- profile.blade.phpの@yield('title')に'MYプロフィール'を埋め込む --}}
 @section('title', 'MYプロフィール')
 @section('content')
+<div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>MYプロフィール</h2>
+            </div>
+        </div>
+    </div>
 @endsection
