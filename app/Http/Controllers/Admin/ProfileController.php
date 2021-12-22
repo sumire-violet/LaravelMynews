de765ff15bc675760fad9832fa4206d550efa29b
sumire-violet <blackout.violet.k@gmail.com>
2021-12-22 04:31:09 +0000

    23章課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+//Profiles Modelが使えるように追記
+use App\Profiles;
+
 class ProfileController extends Controller
 {
     //
@@ -12,9 +15,16 @@ class ProfileController extends Controller
     {
         return view('admin.profile.create');
     }
-
-    public function create()
+    public function create(Request $request)
     {
+      // Varidationを行う
+      $this->validate($request, Profiles::$rules);
+      $profiles = new Profiles;
+      $form = $request->all();
+      // データベースに保存する
+      $profiles->fill($form);
+      $profiles->save();
+    //admin/profile/createにリダイレクトする
         return redirect('admin/profile/create');
     }
 
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -24,19 +24,19 @@
                 <div class="form-group row">
                         <label class="col-md-2">性別</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="gender" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="gender" value="{{ old('gender') }}">
                         </div>
                 </div>
                 <div class="form-group row">
                         <label class="col-md-2">趣味</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="hobby" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
                         </div>
                 </div>    
                 <div class="form-group row">
                         <label class="col-md-2">自己紹介欄</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="self introduce" rows="5">{{ old('self introduce') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="5">{{ old('introduction') }}</textarea>
                         </div>
                 </div>
                  {{ csrf_field() }}
