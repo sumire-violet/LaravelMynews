7342446b8136750d69661f2418f67e52614cf442
sumire-violet <blackout.violet.k@gmail.com>
2022-01-10 10:43:06 +0000

    29章カリキュラム修正

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 //Profiles Modelが使えるように追記
-use App\Profiles;
+use App\Profile;
 use App\ProfileHistory;
 use Carbon\Carbon;
 
@@ -20,8 +20,8 @@ class ProfileController extends Controller
     public function create(Request $request)
     {
       // Varidationを行う
-      $this->validate($request, Profiles::$rules);
-      $profile = new Profiles;
+      $this->validate($request, Profile::$rules);
+      $profile = new Profile;
       $form = $request->all();
       // データベースに保存する
       $profile->fill($form);
@@ -31,13 +31,13 @@ class ProfileController extends Controller
     }
     public function index(Request $request)
     {
-          $posts = Profiles::all();
+          $posts = Profile::all();
       return view('admin.profile.index', ['posts' => $posts]);
     }
     public function edit(Request $request)
     {
          // News Modelからデータを取得する
-      $profile = Profiles::find($request->id);
+      $profile = Profile::find($request->id);
       if (empty($profile)) {
         abort(404);    
       }
@@ -47,9 +47,9 @@ class ProfileController extends Controller
     public function update(Request $request)
     {
           // Validationをかける
-      $this->validate($request, Profiles::$rules);
+      $this->validate($request, Profile::$rules);
       // News Modelからデータを取得する
-      $profile = Profiles::find($request->id);
+      $profile = Profile::find($request->id);
       // 送信されてきたフォームデータを格納する
       $profile_form = $request->all();
       unset($profile_form['_token']);
@@ -67,7 +67,7 @@ class ProfileController extends Controller
     public function delete(Request $request)
   {
       // 該当するNews Modelを取得
-      $profile = Profiles::find($request->id);
+      $profile = Profile::find($request->id);
       // 削除する
       $profile->delete();
       return redirect('admin/profile/');
--- app/Profile.php
+++ app/Profile.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Profiles extends Model
+class Profile extends Model
 {
     //
     // 以下を追記
