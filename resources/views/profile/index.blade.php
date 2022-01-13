49df310997bdfca457153e6fd3161ed48542fab9
sumire-violet <blackout.violet.k@gmail.com>
2022-01-13 11:40:38 +0000

    30章課題2

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -10,7 +10,7 @@
                         <div class="col-md-6">
                             <div class="caption mx-auto">
                                 <div class="title p-2">
-                                    <p>テスト</p>
+                                    <p>テストテストテスト</p>
                                     <h1>{{ str_limit($headline->name, 20) }}</h1>
                                 </div>
                             </div>
